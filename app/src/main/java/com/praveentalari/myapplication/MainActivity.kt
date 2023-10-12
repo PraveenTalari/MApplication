2cfda42c1be99a6f86d687a821eb3ec5242c215f
PT-TC <praveen.talari@transactcampus.com>
2023-10-12 13:20:34 +0100

    first

---
+++ app/src/main/java/com/praveentalari/myapplication/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.praveentalari.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.praveentalari.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val messages: List<Message> = listOf(
+                        Message("first"),
+                        Message("second")
+                    )
+                    Column {
+                        messages.forEach { message ->
+                            Greeting(message.name)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class Message(val name: String)
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
